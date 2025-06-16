40c36c1dce9a49ac28909d0c1a33028af59b12f0
Murali <muralidharrsvm7@gmail.com>
2025-06-16 22:47:47 +0200

    save progress

--- src/interpolation/elph_frohlich_kernels.c
+++ src/interpolation/elph_frohlich_kernels.c
@@ -217,3 +217,103 @@ static void long_range_3D_kernel(const ELPH_float* qplusG,
 
     return;
 }
+
+static void long_range_2D_kernel(
+    const ELPH_float* qplusG, const ELPH_float* Zval, const ELPH_float* Zborn_k,
+    const ELPH_float* Qpole_k, const ELPH_float* epslion,
+    const ELPH_float* tau_k, const ELPH_float zlat, ELPH_cmplx* out_buf)
+{
+    // Compute long range part of the electron-phonon matrix elements for 2D
+    // case. Zval -> valence charge (for monopole) Z_born_k-> born charges (for
+    // dipole) Qpole_k -> Quadrupoles In general we donot need monolope when we
+    // considered total dVSCF i.e dV_ext + dVHa_xc. But in Q.E, only dVHa_xc is
+    // considered, so sometime we need to subtract the monopole (if there is
+    // external potential this will cancell the monopole long range compoment)
+    //
+    // G space dipole term for frohlich in 2D
+    // T Deng et. al Phys. Rev. B 103, 075410 (2021)
+    // T Sohier et. al  Phys. Rev. B 96, 075448 (2017)
+
+    // tau_k are atomic coordinates of atom k in cart
+    // q+G in cart with 2*pi included ,
+    // Zborn_k, born charges for atom k
+    // zlat : lattice parameter along z parameter.
+
+    if (!Zval && !Zborn_k && !Qpole_k)
+    {
+        return;
+    }
+    //
+    ELPH_float q_G_square = dot3_macro(qplusG, qplusG);
+    //
+    if (sqrt(q_G_square) < ELPH_EPS)
+    {
+        // skip q+G = 0 term
+        return;
+    }
+    // struture factor
+    ELPH_cmplx qdot_tau = cexp(-I * (dot3_macro(qplusG, tau_k)));
+
+    ELPH_float Gp_norm = sqrt(qplusG[0] * qplusG[0] + qplusG[1] * qplusG[1]);
+    ELPH_float qGp = zlat * Gp_norm;
+    ELPH_float cos_sin_fac = cos(qplusG[2] * zlat * 0.5);
+    if (Gp_norm > ELPH_EPS)
+    {
+        cos_sin_fac =
+            cos_sin_fac - sin(qplusG[2] * zlat * 0.5) * qplusG[2] / Gp_norm;
+    }
+    ELPH_float cutoff_fac = 1 - exp(-qGp * 0.5) * cos_sin_fac;
+
+    ELPH_float q_eps_q = q_G_square;
+    if (epslion)
+    {
+        // compute (q+G).eps.(q+G)
+        ELPH_float tmp_buf[3] = {0.0, 0.0, 0.0};
+        MatVec3f(epslion, qplusG, false, tmp_buf);
+        q_eps_q = dot3_macro(tmp_buf, qplusG);
+    }
+
+    ELPH_float Zval_buf[3] = {0.0, 0.0, 0.0};
+    ELPH_float Zborn_buf[3] = {0.0, 0.0, 0.0};
+    ELPH_float Qpole_buf[3] = {0.0, 0.0, 0.0};
+    //
+    if (Zval)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            Zval_buf[i] = -(*Zval) * qplusG[i] / q_G_square;
+        }
+    }
+    // compute (q+G).Z
+    if (Zborn_k)
+    {
+        MatVec3f(Zborn_k, qplusG, true, Zborn_buf);
+    }
+    // compute (q+G)_x Q_xyz * (q+G)_y
+    if (Qpole_k)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                for (int k = 0; k < 3; ++k)
+                {
+                    Qpole_buf[k] =
+                        Qpole_buf[k] +
+                        qplusG[i] * qplusG[j] * Qpole_k[k + 3 * j + 9 * i];
+                }
+            }
+        }
+    }
+    // compute and multiply with a decay factor
+    qdot_tau *= exp(-q_G_square * 0.25);
+
+    for (int i = 0; i < 3; ++i)
+    {
+        out_buf[i] =
+            qdot_tau *
+            ((Zborn_buf[i] - I * 0.5 * Qpole_buf[i]) / q_eps_q + Zval_buf[i]);
+    }
+
+    return;
+}
