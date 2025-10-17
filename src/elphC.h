6d8f9a85e957e87b23bab3a0da42ef738cb2d587
Murali <muralidharrsvm7@gmail.com>
2025-10-17 23:39:43 +0200

    add docs elphC.h

--- src/elphC.h
+++ src/elphC.h
@@ -21,7 +21,7 @@
 /**
  * @typedef ND_int
  * @brief Standard integer type for array dimensions and indices
- *
+ * 
  * Defined as a 64-bit signed integer to support large array dimensions
  */
 typedef long long int ND_int;
@@ -39,19 +39,60 @@ typedef long long int ND_int;
  * @brief Floating-point type (double precision when COMPILE_ELPH_DOUBLE is defined)
  */
 typedef double ELPH_float;
+
+/**
+ * @typedef ELPH_cmplx
+ * @brief Complex number type (double precision when COMPILE_ELPH_DOUBLE is defined)
+ */
 typedef double _Complex ELPH_cmplx;
 
+/**
+ * @def ELPH_MPI_float
+ * @brief MPI datatype for ELPH_float (double precision)
+ */
 #define ELPH_MPI_float MPI_DOUBLE
+
+/**
+ * @def ELPH_MPI_cmplx
+ * @brief MPI datatype for ELPH_cmplx (double precision complex)
+ */
 #define ELPH_MPI_cmplx MPI_C_DOUBLE_COMPLEX
 
+/**
+ * @def ELPH_NC4_IO_FLOAT
+ * @brief NetCDF datatype for ELPH_float (double precision)
+ */
 #define ELPH_NC4_IO_FLOAT NC_DOUBLE
+
 #else
+/**
+ * @typedef ELPH_float
+ * @brief Floating-point type (single precision by default)
+ */
 typedef float ELPH_float;
+
+/**
+ * @typedef ELPH_cmplx
+ * @brief Complex number type (single precision by default)
+ */
 typedef float _Complex ELPH_cmplx;
 
+/**
+ * @def ELPH_MPI_float
+ * @brief MPI datatype for ELPH_float (single precision)
+ */
 #define ELPH_MPI_float MPI_FLOAT
+
+/**
+ * @def ELPH_MPI_cmplx
+ * @brief MPI datatype for ELPH_cmplx (single precision complex)
+ */
 #define ELPH_MPI_cmplx MPI_C_FLOAT_COMPLEX
 
+/**
+ * @def ELPH_NC4_IO_FLOAT
+ * @brief NetCDF datatype for ELPH_float (single precision)
+ */
 #define ELPH_NC4_IO_FLOAT NC_FLOAT
-#endif
 
+#endif
